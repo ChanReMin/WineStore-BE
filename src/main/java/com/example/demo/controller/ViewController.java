9dfda8ab8dabdd5d201360e2269b1ba8cec05cf2
leminh <minhletran152003@gmail.com>
2025-11-15 11:24:57 +0700

    Initiate Projects

---
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "index";
+    }
+}
