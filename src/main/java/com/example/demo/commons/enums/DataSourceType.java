e6b868216dd8e522e2db4c2dbed46022697609d7
Vanthuyen <125105464+Vanthuyen@users.noreply.github.com>
2025-11-18 15:46:40 +0700

    feat: fix cqrs

---
+++ src/main/java/com/example/demo/commons/enums/DataSourceType.java
@@ -0,0 +1,6 @@
+package com.example.demo.commons.enums;
+
+public enum DataSourceType {
+    WRITE,
+    READ
+}
---
+++ src/main/java/com/example/demo/configs/DataSourceConfig.java
@@ -0,0 +1,93 @@
+package com.example.demo.configs;
+
+import com.example.demo.commons.enums.DataSourceType;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.write")
+    public DataSourceProperties writeDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.write.hikari")
+    public DataSource writeDataSource() {
+        return writeDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    // Read DataSource Configuration
+    @Bean
+    @ConfigurationProperties("spring.datasource.read")
+    public DataSourceProperties readDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.read.hikari")
+    public DataSource readDataSource() {
+        return readDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    // Routing DataSource
+    @Bean
+    public DataSource routingDataSource(
+            @Qualifier("writeDataSource") DataSource writeDataSource,
+            @Qualifier("readDataSource") DataSource readDataSource) {
+
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put(DataSourceType.WRITE, writeDataSource);
+        dataSourceMap.put(DataSourceType.READ, readDataSource);
+
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(writeDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("routingDataSource") DataSource dataSource) {
+
+        return builder
+                .dataSource(dataSource)
+                .packages("com.example.demo.entities")
+                .persistenceUnit("default")
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+}
