4224f64efe18eab714360eb946a11168fd0eafa9
Tuan Cuong <tuancuong06062002@gmail.com>
2025-11-29 14:06:55 +0700

    create: promotion feature

---
+++ src/main/java/com/example/demo/dtos/responses/promotion/CreatePromotionResponse.java
@@ -0,0 +1,18 @@
+package com.example.demo.dtos.responses.promotion;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreatePromotionResponse {
+    private Long id;
+    private String code;
+    private String name;
+    private Integer status;
+    private LocalDateTime created_at;
+}
