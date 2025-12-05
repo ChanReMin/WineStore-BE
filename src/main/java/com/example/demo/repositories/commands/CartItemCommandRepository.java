42e45b674b769e98b718bd3352656e3de1ac77d4
Vanthuyen <125105464+Vanthuyen@users.noreply.github.com>
2025-12-05 13:09:21 +0700

    fix: fix bug 500 Invalid password

--- src/main/java/com/example/demo/repositories/commands/CartItemCommandRepository.java
+++ src/main/java/com/example/demo/repositories/commands/CartItemCommandRepository.java
@@ -2,8 +2,17 @@ package com.example.demo.repositories.commands;
 
 import com.example.demo.entities.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CartItemCommandRepository extends JpaRepository<CartItem, Long> {
+    @Query(value = "SELECT * FROM cart_items WHERE cart_id = :cartId AND product_id = :productId",
+            nativeQuery = true)
+    Optional<CartItem> findByCartIdAndProductIdIncludingDeleted(
+            @Param("cartId") Long cartId,
+            @Param("productId") Long productId);
 }
